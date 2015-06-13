5f0bb4691428c3c7f17fc7327c48a65ff5030358
Kirk Woll <kirk.woll@gmail.com>
2015-06-12 19:22:11 -0700

    Add nuget stuff

---
+++ makepackage.bat
@@ -0,0 +1,13 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" AsyncProxy.sln
+
+cd Nuget
+mkdir lib
+mkdir lib\net45
+
+copy ..\NetInjector\bin\Debug\NetInjector.* lib\net45
+
+nuget pack NetInjector.nuspec
+
+rmdir lib /S /Q
+
+cd ..
