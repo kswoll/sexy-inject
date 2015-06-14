26a5057e4408d7bd213775c24d869bf90b56caac
Kirk Woll <kirk.woll@gmail.com>
2015-06-14 09:32:50 -0700

    Add NugetUtilities for the build server

--- makepackage.bat
+++ makepackage.bat
@@ -1,13 +1,17 @@
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" NetInjector.sln
 
 cd Nuget
+mkdir build
+cd build
 mkdir lib
 mkdir lib\net45
 
-copy ..\NetInjector\bin\Debug\NetInjector.* lib\net45
+copy ..\..\NetInjector\bin\Debug\NetInjector.* lib\net45
 
-nuget pack NetInjector.nuspec
+copy ..\NetInjector.nuspec .
+..\nuget pack NetInjector.nuspec
+copy *.nupkg ..
 
-rmdir lib /S /Q
-
+cd..
+rmdir build /S /Q
 cd ..
